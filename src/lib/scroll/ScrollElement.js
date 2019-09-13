7cf5609c62b1667174e5fd1bd88c082bf7cf5054
Pawel Olszewski <pawel.olszewski@droptica.pl>
2019-09-13 15:17:51 +0200

    rct-scroll height fix

--- src/lib/scroll/ScrollElement.js
+++ src/lib/scroll/ScrollElement.js
@@ -177,7 +177,7 @@ class ScrollElement extends Component {
 
     const scrollComponentStyle = {
       width: `${width}px`,
-      height: `${height + 20}px`, //20px to push the scroll element down off screen...?
+      height: `${height}px`,
       cursor: isDragging ? 'move' : 'default',
       position: 'relative'
     }
